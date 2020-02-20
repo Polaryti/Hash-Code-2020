642c6401a13d145ee7e614e7caefc77366c2a274
PereMarco <48091152+PereMarco@users.noreply.github.com>
2020-02-20 19:24:15 +0100

    Clase Libo y Libreria actualizada

---
+++ OnlineRound/Structures/Libro.java
@@ -0,0 +1,13 @@
+package Structures;
+
+public class Libro{
+    public int id;
+    public int puntos;
+
+    public Libro(int id, int puntuacion){
+        this.id = id;
+        puntos = puntuacion;
+    }
+    
+
+}
